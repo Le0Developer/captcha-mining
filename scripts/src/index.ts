b3565b17922b554c3460ac0d12e0bd4ff8a7f0ff
Leo Developer <leo@git.leodev.win>
2025-04-01 10:53:32 +0200

    add vercel security

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,8 +1,10 @@
 import { updateBunnyShield } from "./bunny-shield";
 import { updateHCaptcha } from "./hcaptcha";
 import { updateTurnstile } from "./turnstile";
+import { updateVercelSecurity } from "./vercel";
 
 await updateTurnstile();
 await updateHCaptcha();
 await updateBunnyShield();
+await updateVercelSecurity();
 
---
+++ scripts/src/vercel.ts
@@ -0,0 +1,71 @@
+import path from "node:path";
+import { Buffer } from "node:buffer";
+
+import { alert, notify } from "./lib/notifications";
+import fs from "fs-extra";
+import { tryAndPush } from "./lib/git";
+import { cleanJavascript } from "./lib/clean";
+
+const dir = path.resolve("../vercel-security");
+await fs.ensureDir(dir);
+
+const assets = [
+  "challenge.v2.min.js",
+  "challenge.v2.wasm",
+]
+
+export async function updateVercelSecurity() {
+  const files: Array<{
+    file: string;
+    buffer: Buffer;
+  }> = [];
+  for (const file of assets) {
+    const res = await fetch(`https:///vercel.com/.well-known/vercel/security/static/${file}`);
+    const buffer = Buffer.from(await res.arrayBuffer());
+    if (!res.ok) {
+      await alert(`Failed to fetch ${file}: ${res.status} ${buffer}`);
+      continue;
+    }
+    if (file.endsWith(".js")) {
+      const text = buffer.toString("utf-8");
+      const cleaned = await cleanJavascript(text);
+      files.push({ file, buffer: Buffer.from(cleaned) });
+    } else {
+      files.push({ file, buffer: buffer });
+    }
+  }
+
+  const fullBytes = new Uint8Array(files.reduce((acc, { buffer }) => acc + buffer.length, 0) + files.length);
+  let offset = 0;
+  for (const { buffer } of files) {
+    fullBytes.set(buffer, offset);
+    fullBytes[offset + buffer.length] = 0x00;
+    offset += buffer.length + 1;
+  }
+
+  const version = Buffer.from(new Uint8Array(await crypto.subtle.digest("SHA-256", fullBytes))).toString("hex").slice(0, 16);
+  console.log(`Vercel Security version: ${version}`);
+
+  const versionDir = path.join(dir, "archive", version);
+  const current = path.join(dir, "current");
+  if (await fs.pathExists(versionDir) && await fs.pathExists(current)) {
+    console.log("Already got this version");
+    return;
+  }
+
+  await fs.ensureDir(versionDir);
+  await fs.ensureDir(current);
+
+  for (const { file, buffer } of files) {
+    await fs.writeFile(path.join(versionDir, file), buffer);
+    await fs.writeFile(path.join(current, file), buffer);
+  }
+
+  const url = await tryAndPush(assets.map((name) => [
+    path.join(current, name),
+    path.join(versionDir, name),
+  ]).flat(), `Vercel Security version: ${version}`);
+
+  await notify(`Vercel Security updated to version ${version}: ${url}`);
+}
+

ad030e40d591e3fbc42d013042548c7ceb67ebdb
Leo Developer <leo@git.leodev.win>
2025-04-01 10:22:07 +0200

    run webcrack multiple times

--- scripts/src/lib/clean.ts
+++ scripts/src/lib/clean.ts
@@ -1,11 +1,25 @@
 import { webcrack } from "./webcrack";
 
 export async function cleanJavascript(js: string, demangle: boolean = true): Promise<string> {
+  while (true) {
+    const cracked = await webcrack(js, {
+      unpack: true,
+      unminify: true,
+      deobfuscate: true,
+    })
+    if (cracked.code === js)
+      break;
+    js = cracked.code;
+  }
+  if (!demangle) {
+    return js
+  }
+
   const res = await webcrack(js, {
     unpack: true,
     unminify: true,
     deobfuscate: true,
-    mangle: demangle ? "stable" : undefined,
+    mangle: "stable",
   })
   return res.code
 }

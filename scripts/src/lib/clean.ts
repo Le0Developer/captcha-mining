268ef5f1a9b969f204aaa66dfa74d1d372471818
Leo Developer <leo@git.leodev.win>
2025-04-08 09:40:01 +0200

    fix potential loop in cleanjs

--- scripts/src/lib/clean.ts
+++ scripts/src/lib/clean.ts
@@ -1,15 +1,17 @@
 import { webcrack } from "./webcrack";
 
 export async function cleanJavascript(js: string, demangle: boolean = true): Promise<string> {
+  const versions = new Set();
   while (true) {
     const cracked = await webcrack(js, {
       unpack: true,
       unminify: true,
       deobfuscate: true,
     })
-    if (cracked.code === js)
+    if (cracked.code === js || versions.has(cracked.code))
       break;
     js = cracked.code;
+    versions.add(js);
   }
   if (!demangle) {
     return js

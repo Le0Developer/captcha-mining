8e9c1ec368a5f13c25a7c8a561f136a5035da7b8
Leo Developer <leo@git.leodev.win>
2025-08-29 15:09:38 +0200

    add top level error handler

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,12 +1,16 @@
 import { updateAWSWAF } from "./awswaf";
 import { updateBunnyShield } from "./bunny-shield";
 import { updateHCaptcha } from "./hcaptcha";
+import { alert } from "./lib/notifications";
 import { updateTurnstile } from "./turnstile";
 import { updateVercelSecurity } from "./vercel";
 
-await updateTurnstile();
-await updateHCaptcha();
-await updateBunnyShield();
-await updateVercelSecurity();
-await updateAWSWAF();
-
+try {
+  await updateTurnstile();
+  await updateHCaptcha();
+  await updateBunnyShield();
+  await updateVercelSecurity();
+  await updateAWSWAF();
+} catch (err) {
+  await alert(`Error in main execution: ${err}`);
+}
